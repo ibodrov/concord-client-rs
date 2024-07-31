450b3b85b5dbf86f8a3bcb5fd0c5bb2e682cf057
Ivan Bodrov <ibodrov@gmail.com>
2024-07-30 21:39:30 -0400

    handle keep alive requests, add config

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use std::fmt::Debug;
 
 use futures_util::{SinkExt, StreamExt};
-use http::{Request, Uri};
+use http::{
+    header::{
+        AUTHORIZATION, CONNECTION, HOST, SEC_WEBSOCKET_KEY, SEC_WEBSOCKET_VERSION, UPGRADE,
+        USER_AGENT,
+    },
+    Request, Uri,
+};
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpStream;
 use tokio_tungstenite::{
@@ -83,6 +89,13 @@ pub enum Message {
         process_id: Uuid,
         // TODO imports
     },
+    Ping,
+    Pong,
+}
+
+pub struct Config {
+    pub uri: Uri,
+    pub auth_header: String,
 }
 
 pub struct QueueClient {
@@ -90,14 +103,10 @@ pub struct QueueClient {
 }
 
 impl QueueClient {
-    pub async fn connect(uri: Uri, auth_header: String) -> Result<Self, ApiError> {
-        let req = QueueClient::create_connect_request(uri.clone(), auth_header)?;
+    pub async fn connect(config: Config) -> Result<Self, ApiError> {
+        let req = QueueClient::create_connect_request(&config.uri, &config.auth_header)?;
 
-        let (ws_stream, _) = tokio_tungstenite::connect_async(req)
-            .await
-            .map_err(|e| ApiError {
-                message: e.to_string(),
-            })?;
+        let (ws_stream, _) = tokio_tungstenite::connect_async(req).await?;
 
         Ok(QueueClient { ws_stream })
     }
@@ -109,32 +118,29 @@ impl QueueClient {
         Ok(())
     }
 
-    pub async fn receive_message(&mut self) -> Result<Option<Message>, ApiError> {
+    pub async fn receive_message(&mut self) -> Result<Message, ApiError> {
         let msg = self
             .ws_stream
             .next()
             .await
             .ok_or_else(|| ApiError::simple("No message received (possibly a bug)"))?;
 
-        let text = match msg? {
-            tungstenite::Message::Text(text) => text,
-            tungstenite::Message::Ping(_) => {
-                self.ws_stream
-                    .send(tungstenite::Message::Pong(vec![]))
-                    .await?;
-                return Ok(None);
+        let msg = match msg? {
+            tungstenite::Message::Text(text) => serde_json::from_str::<Message>(&text)?,
+            tungstenite::Message::Ping(_) => Message::Ping,
+            tungstenite::Message::Pong(_) => Message::Pong,
+            msg => {
+                return Err(ApiError::simple(&format!(
+                    "Unexpected message type (possibly a bug): {:?}",
+                    msg
+                )))
             }
-            _ => return Err(ApiError::simple("Unexpected message type (possibly a bug)")),
         };
 
-        let msg = serde_json::from_str::<Message>(&text)?;
-        Ok(Some(msg))
+        Ok(msg)
     }
 
-    fn create_connect_request(
-        uri: Uri,
-        auth_header: String,
-    ) -> Result<http::Request<()>, ApiError> {
+    fn create_connect_request(uri: &Uri, auth_header: &str) -> Result<http::Request<()>, ApiError> {
         let host = format!(
             "{}:{}",
             uri.host().unwrap_or("localhost"),
@@ -145,13 +151,16 @@ impl QueueClient {
 
         Request::builder()
             .uri(uri.clone())
-            .header("Host", host)
-            .header("Authorization", auth_header)
-            .header("Connection", "Upgrade")
-            .header("Upgrade", "websocket")
-            .header("Sec-WebSocket-Version", "13")
-            .header("Sec-WebSocket-Key", ws_key)
-            .header("User-Agent", "runtime-rs")
+            .header(HOST, host)
+            .header(AUTHORIZATION, auth_header)
+            .header(CONNECTION, "Upgrade")
+            .header(UPGRADE, "websocket")
+            .header(SEC_WEBSOCKET_VERSION, "13")
+            .header(SEC_WEBSOCKET_KEY, ws_key)
+            .header(
+                USER_AGENT,
+                format!("concord-client-rs/{}", env!("CARGO_PKG_VERSION")),
+            )
             .body(())
             .map_err(|e| ApiError {
                 message: e.to_string(),
